911100f3f5e60ad401449fff63c8eec7862d3197
Anne WALCH <awalch@talaron.com>
2026-06-09 16:36:22 +0200

    Add typed API GET and POST tests

--- tests/api/api.spec.ts
+++ tests/api/api.spec.ts
@@ -1,11 +1,14 @@
 import { test, request, expect } from "@playwright/test";
 import { Post } from "../../interfaces/Post";
+import posts from "../../test-data/posts.json";
+import { PostPayload } from "../../interfaces/PostPayload";
 
+// sans typage des données
 test("Get users from JSONPlaceholder API", async ({ request }) => {
   const response = await request.get(
     "https://jsonplaceholder.typicode.com/posts",
   );
-  expect(response.ok()).toBeTruthy;
+  expect(response.ok()).toBeTruthy();
 
   const body = await response.json();
   //console.log(body);
@@ -17,6 +20,26 @@ test("Get users from JSONPlaceholder API", async ({ request }) => {
   expect(typeof body[0].title).toBe("string");
 });
 
+// avec typage des données via interface Post
+test("Get users from JSONPlaceholder API through interface", async ({
+  request,
+}) => {
+  const response = await request.get(
+    "https://jsonplaceholder.typicode.com/posts",
+  );
+  expect(response.ok()).toBeTruthy();
+
+  const body: Post[] = await response.json();
+  //console.log(body);
+  expect(body.length).toBeGreaterThan(0);
+
+  expect(body[0].id).toBe(1);
+  expect(body[0]).toHaveProperty("title");
+  expect(body[0]).toHaveProperty("body");
+  // expect(typeof body[0].title).toBe("string");   inutile avec typage
+});
+
+// sans typage des données
 test("create POST with JSONPlaceholder API", async ({ request }) => {
   const response = await request.post(
     "https://jsonplaceholder.typicode.com/posts",
@@ -28,13 +51,16 @@ test("create POST with JSONPlaceholder API", async ({ request }) => {
       },
     },
   );
-  expect(response.ok()).toBeTruthy;  // moins pertinent que le suivant
+  expect(response.ok()).toBeTruthy(); // moins pertinent que le suivant
   expect(response.status()).toBe(201);
   const body = await response.json();
-  expect(body).toMatchObject({userId:666, title:"Book Title"});
+  expect(body).toMatchObject({ userId: 666, title: "Book Title" });
   expect(typeof body.userId).toBe("number");
+  expect(typeof body.title).toBe("string");
+  expect(typeof body.body).toBe("string");
 });
 
+// Avec type des données via l'interface Post
 test("create POST 2 with JSONPlaceholder API", async ({ request }) => {
   const response = await request.post(
     "https://jsonplaceholder.typicode.com/posts",
@@ -48,10 +74,37 @@ test("create POST 2 with JSONPlaceholder API", async ({ request }) => {
   );
 
   expect(response.status()).toBe(201);
+
   const body: Post = await response.json();
-  expect(body).toMatchObject({userId:123, title: "Loving API power",body: "this is a very long body"});
-  expect(typeof body.userId).toBe("number");
+  expect(body).toMatchObject({
+    userId: 123,
+    title: "Loving API power",
+    body: "this is a very long body",
+  });
+
+  expect(body.id).toBeDefined();
+
+  // inutile avec le typage des données issu de l'interface Post
+  /*   expect(typeof body.userId).toBe("number");
   expect(typeof body.title).toBe("string");
   expect(typeof body.body).toBe("string");
+ */
 });
 
+// avec typage et jeux de données
+test("create POST 3 with JSONPlaceholder API", async ({ request }) => {
+  const payload: PostPayload = posts.validPost;
+
+  const response = await request.post(
+    "https://jsonplaceholder.typicode.com/posts",
+    { data: payload }
+  );
+
+  expect(response.status()).toBe(201);
+
+  const body: Post = await response.json();
+  // console.log(body);
+  expect(body.id).toBeDefined();
+  expect(body).toMatchObject(posts.validPost);
+
+});

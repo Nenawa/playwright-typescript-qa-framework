d850981b7b1906461c4199026177468d53e95d01
Anne WALCH <awalch@talaron.com>
2026-06-17 15:33:33 +0200

    Edit playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,6 +24,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
  reporter: [
     ['html', { outputFolder: 'playwright-report', open: 'never' }],
+    ['github'],
     ['list'],  // logs dans la console CI
   ],  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {

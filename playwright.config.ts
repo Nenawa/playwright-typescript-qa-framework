974f7b726d535fb0d72d66af1734eb5234b01b2a
Anne WALCH <awalch@talaron.com>
2026-06-17 15:00:56 +0200

    Add file tsconfig.json

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,8 +22,10 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+ reporter: [
+    ['html', { outputFolder: 'playwright-report', open: 'never' }],
+    ['list'],  // logs dans la console CI
+  ],  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',

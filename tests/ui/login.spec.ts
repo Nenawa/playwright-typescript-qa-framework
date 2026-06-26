5475c98f1b8cfcc5633b509bfb8c3999c9ef21ab
Anne WALCH <awalch@talaron.com>
2026-06-26 15:44:00 +0200

    refactor implement data-driven login tests

--- tests/ui/login.spec.ts
+++ tests/ui/login.spec.ts
@@ -2,11 +2,30 @@ import test, { expect } from "@playwright/test";
 import { LoginPage } from "../../pages/LoginPage";
 import users from "../../test-data/users.json";
 
-test("successful login", async ({ page }) => {
-  const loginPage = new LoginPage(page);
+test.describe("Login", () => {
+  // test passant
+  const validUsers = users.validUser;
+  for (const validUser of validUsers) {
+    test(`successful login - ${validUser.username}`, async ({ page }) => {
+      const loginPage = new LoginPage(page);
 
-  await loginPage.goto();
-  await loginPage.login(users.validUser);
+      await loginPage.goto();
+      await loginPage.login(validUser);
 
-  await expect(page).toHaveURL(/inventory/);
+      await expect(page).toHaveURL(/inventory/);
+    });
+  }
+
+  // Test non passant
+  const invalidUsers = users.invalidUser;
+  for (const invalidUser of invalidUsers) {
+    test(`Unsuccessful login - ${invalidUser.username}`, async ({ page }) => {
+      const loginPage = new LoginPage(page);
+
+      await loginPage.goto();
+      await loginPage.login(invalidUser);
+
+      await expect(page).toHaveURL(/inventory/);
+    });
+  }
 });
